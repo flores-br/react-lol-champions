a35a6c672d79ab9b00bcf0410962c8b886e55859
Bryan Flores <bryanflores12345@gmail.com>
2022-06-15 15:30:16 -0400

    cards without background

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,57 @@
 import Card from './components/Card';
 import Header from './components/Header';
+import axios from 'axios';
+import { useState, useEffect } from 'react';
+
+const imgUrl =
+  'https://ddragon.leagueoflegends.com/cdn/img/champion/loading/Rammus_0.jpg';
 
 const App = () => {
+  const [data, setData] = useState([]);
+
+  // get all champion names
+  const getNames = async () => {
+    try {
+      const response = await axios
+        .get(
+          'http://ddragon.leagueoflegends.com/cdn/12.11.1/data/en_US/champion.json'
+        )
+        .then(res => Object.keys(res.data.data));
+
+      let champData = [];
+
+      for (const n of response) {
+        const res = await axios
+          .get(
+            `http://ddragon.leagueoflegends.com/cdn/12.11.1/data/en_US/champion/${n}.json`
+          )
+          .then(r => r['data']['data'][`${n}`]);
+        res[
+          'image'
+        ] = `http://ddragon.leagueoflegends.com/cdn/img/champion/loading/${n}_0.jpg`;
+
+        const obj = {
+          champion: `${n}`,
+          data: res,
+        };
+
+        champData.push(obj);
+      }
+
+      setData(champData);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  useEffect(() => {
+    getNames();
+  }, []);
+
   return (
     <>
       <Header />
-      <Card />
+      <Card data={data} />
     </>
   );
 };
--- client/src/components/Card/index.js
+++ client/src/components/Card/index.js
@@ -1,60 +1,36 @@
 import './styles.css';
-import axios from 'axios';
-import { useState, useEffect } from 'react';
 
-const Card = () => {
-  const imgUrl =
-    'https://ddragon.leagueoflegends.com/cdn/img/champion/loading/Rammus_0.jpg';
-
-  const [data, setData] = useState([]);
-
-  // load all data on first load
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const res = await axios.get(
-          'http://ddragon.leagueoflegends.com/cdn/12.11.1/data/en_US/champion.json'
-        );
-        setData(Object.keys(res['data']['data']));
-      } catch (err) {
-        console.log(err);
-      }
-    };
-
-    fetchData();
-  }, []);
-
+const Card = ({ data }) => {
   return (
     <div className="cards">
-      <div className="card" style={{ backgroundImage: `url(${imgUrl})` }}>
-        <div className="card-content">
-          <h1 className="card-title">Rammus</h1>
-          <h2 className="card-subtitle">the Armordillo</h2>
-          <h3 className="card-class">Champion Class</h3>
-          <p className="card-body">
-            Idolized by many, dismissed by some, mystifying to all, the curious
-            being Rammus is an enigma. Protected by a spiked shell, he inspires
-            increasingly disparate theories on his origin wherever he goes—from
-            demigod, to sacred oracle, to a mere beast transformed by magic.
-            Whatever the truth may be, Rammus keeps his own counsel and stops
-            for no one as he roams the Shuriman desert.
-          </p>
-          <div className="stats">
-            <div className="one-third">
-              <div className="stat-value">8</div>
-              <div className="stat-name">Attack</div>
-            </div>
-            <div className="one-third">
-              <div className="stat-value">4</div>
-              <div className="stat-name">Defense</div>
-            </div>
-            <div className="one-third no-border">
-              <div className="stat-value">3</div>
-              <div className="stat-name">Magic</div>
+      {data.map(d => (
+        <div
+          className="card"
+          key={d.data.key}
+          style={{ backgroundImage: d.data.image }}
+        >
+          <div className="card-content">
+            <h1 className="card-title">{d.data.name}</h1>
+            <h2 className="card-subtitle">{d.data.title}</h2>
+            <h3 className="card-class">{d.data.tags[0]}</h3>
+            <p className="card-body">{d.data.lore}</p>
+            <div className="stats">
+              <div className="one-third">
+                <div className="stat-value">{d.data.info.attack}</div>
+                <div className="stat-name">Attack</div>
+              </div>
+              <div className="one-third">
+                <div className="stat-value">{d.data.info.defense}</div>
+                <div className="stat-name">Defense</div>
+              </div>
+              <div className="one-third no-border">
+                <div className="stat-value">{d.data.info.magic}</div>
+                <div className="stat-name">Magic</div>
+              </div>
             </div>
           </div>
         </div>
-      </div>
+      ))}
     </div>
   );
 };
